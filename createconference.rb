8a0ddf8528f30421d7e8ba1e72e905ed562c7815
SpringStorm5 <springstorm666@gmail.com>
2016-10-12 00:59:53 +0300

    refactoring createconferense

--- createconference.rb
+++ createconference.rb
@@ -17,18 +17,40 @@ Capybara.default_driver = :chrome
  Capybara::Selenium::Driver.new(app, :browser => :chrome)
  end
 
-Capybara.visit('http://localhost:3000/en/session/new') #переделать
-#Capybara.click_link('Log')
-Capybara.fill_in('login', :with => 'Cialellilunique')
-Capybara.fill_in('password', :with => '6279508')
-Capybara.click_button('Log')
-Capybara.visit('http://localhost:3000/en/admin/conferences') #.//*[@id='sub-menu']/li[2]/ul/li[2]/a
-# Capybara.click_link('Admin')
-# Capybara.visit Capybara.find('a', :text => 'Conferences admin')[:href]
-Capybara.click_link('as_admin__conferences-new--link')
-Capybara.fill_in('record_name_', :with => 'test1')
-Capybara.fill_in('record_start_date_', :with => '2016-11-30')
-Capybara.fill_in('record_finish_date_',:with => '2016-12-14')
-sleep 3
-Capybara.check('record_registration_opened_')
-Capybara.click_button('Create')
+module Createconference
+ class Createconference
+
+  def login(login)
+   @login = login
+  end
+
+  def password(password)
+   @password = password
+  end
+
+   def create
+    Capybara.visit('http://localhost:3000/')
+    Capybara.click_link('Log')
+    Capybara.fill_in('login', :with => @login)
+    Capybara.fill_in('password', :with => @password)
+    Capybara.click_button('Log')
+    Capybara.visit('http://localhost:3000/en/admin/conferences') #.//*[@id='sub-menu']/li[2]/ul/li[2]/a
+    # Capybara.click_link('Admin')
+    # Capybara.visit Capybara.find('a', :text => 'Conferences admin')[:href]
+    Capybara.click_link('as_admin__conferences-new--link')
+    Capybara.fill_in('record_name_', :with => 'test1')
+    Capybara.fill_in('record_start_date_', :with => '2016-11-30')
+    Capybara.fill_in('record_finish_date_',:with => '2016-12-14')
+    sleep 3
+    Capybara.check('record_registration_opened_')
+    Capybara.click_button('Create')
+   end
+ end
+end
+
+c = Createconference::Createconference.new
+
+login =
+password =
+c.create
+
