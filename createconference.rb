91bf42c48fd2fce510fb957cb69ef89294ce4a29
SpringStorm5 <springstorm666@gmail.com>
2016-10-12 09:06:56 +0300

    refactoring and any bug fix

--- createconference.rb
+++ createconference.rb
@@ -17,8 +17,8 @@ Capybara.default_driver = :chrome
  Capybara::Selenium::Driver.new(app, :browser => :chrome)
  end
 
-module Createconference
- class Createconference
+module Cr
+ class Createcl
 
   def login(login)
    @login = login
@@ -31,27 +31,29 @@ module Createconference
    def create
     Capybara.visit('http://localhost:3000/')
     Capybara.click_link('Log')
-    Capybara.fill_in('login', :with => @login)
+    Capybara.fill_in('login', :with => @login) #@login
     Capybara.fill_in('password', :with => @password)
     Capybara.click_button('Log')
     Capybara.visit('http://localhost:3000/en/admin/conferences') #.//*[@id='sub-menu']/li[2]/ul/li[2]/a
     # Capybara.click_link('Admin')
     # Capybara.visit Capybara.find('a', :text => 'Conferences admin')[:href]
     Capybara.click_link('as_admin__conferences-new--link')
-    Capybara.fill_in('record_name_', :with => 'test1')
+    Capybara.fill_in('record_name_', :with => 'test2')
     Capybara.fill_in('record_start_date_', :with => '2016-11-30')
     Capybara.fill_in('record_finish_date_',:with => '2016-12-14')
     sleep 3
     Capybara.check('record_registration_opened_')
     Capybara.click_button('Create')
    end
+
+
  end
 end
 
-c = Createconference::Createconference.new
+c = Cr::Createcl.new
 
-c.login = ''
-c.password = 6279508
+
+c.login('Darling')
+c.password('6279508')
 c.create
-
 
